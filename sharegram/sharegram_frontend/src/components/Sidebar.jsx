d1298f9c5b989efb58c8e2b5a07aba5efbe1176d
mathi <mathieu.huynh@edu.esiee.fr>
2023-04-03 14:33:56 +0200

    update sharegram

--- sharegram/sharegram_frontend/src/components/Sidebar.jsx
+++ sharegram/sharegram_frontend/src/components/Sidebar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link, Routes, Route } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+import logo from '../assets/sharegram_logo.png';
+import { categories } from '../utils/data';
 
-const Sidebar = () => {
+const Sidebar = ( {closeToggle,user}) => {
+
+  const handleCloseSideBar = () => {
+    if (closeToggle)  closeToggle(false);
+  } 
+
   return (
-    <div>
-      Sidebar
+    <div className='flex flex-row h-full bg-white hide-scrollbar min-w-210 overflow-y-scroll'>
+      <div className='absolute flex flex-col '>
+        <Link
+          to="/"
+          className='flex  px-5  my-6 pt-1 w-190 items-center gap-2'
+          onClick={()=> {console.log('hello')}}
+        >
+          <img src={logo} alt="logo" className='w-full '/>
+        </Link>
+        
+
+        <div className='flex flex-col '>
+
+        </div>
+      </div>
     </div>
   )
 }
--- sharegram/sharegram_frontend/src/containers/Home.jsx
+++ sharegram/sharegram_frontend/src/containers/Home.jsx
@@ -16,7 +16,8 @@ const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-  
+  const scrollRef = useRef(null);
+
   useEffect( () =>{
     const query = userQuery(userInfo?.data?.sub)
     
@@ -30,18 +31,38 @@ const Home = () => {
   return (
     <div className=" flex bg-gray-50  md:flex-row flex-col h-screen ease-out transition-height duration-75">
       <div className='hidden bg-gray-500 md:flex h-screen flex-initial '>
-        <Sidebar/>
+      <Sidebar user={user && user} />
       </div>
-      <div className='flex md:hidden flex-row items-center mt-2 '>
-        <HiMenu fontSize={35} className='cursor-ponter' onClick={() => setToggleSidebar(true)} />
+      <div className='flex md:hidden flex-row  '>
+        <div className='w-full flex flex-rox justify-between items-center p-3'>
+        <HiMenu fontSize={35} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
         <Link to="/">
           <img src={logo} alt="logo" className='w-32' />
         </Link>
         <Link to={`user-profil/${user?._id}`}>
           <img src={user?.image} alt="logo" className='w-10 rounded-full ml-2' />
         </Link>
+        </div>
         
+        {toggleSidebar && (
+          <div className="absolute top-0 w-4/5 bg-white h-screen  overflow-y-auto shadow-md  animate-slide-in ">
+            <div className="absolute w-full h-full flex justify-end  p-2 z-0 ">
+              <AiFillCloseCircle fontSize={40} className="cursor-pointer  bg-white " onClick={() => setToggleSidebar(false)} />
+            </div>
+               <Sidebar closeToggle={setToggleSidebar} user={user && user} /> 
+              
+          </div>
+        )
+        }
       </div>
+
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
+
     </div>
   )
 }
