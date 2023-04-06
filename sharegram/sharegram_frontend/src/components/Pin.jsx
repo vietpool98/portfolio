f325020cf4f03767e89c5afd9e4ec37b6868c2d3
Mathieu HUYNH <mathieu.huynh@edu.esiee.fr>
2023-04-06 22:35:21 +0200

    addind button to download the image

--- sharegram/sharegram_frontend/src/components/Pin.jsx
+++ sharegram/sharegram_frontend/src/components/Pin.jsx
@@ -11,10 +11,33 @@ import { client, urlFor } from '../client';
 const Pin = ({pin}) => {
 
  const {image, postedBy,save} = pin
+ const [onImageHover, setOnImageHover] = useState(false);
+ const navigate = useNavigate();
+ 
   return (
-    <div>
-      <img src={pin.image.asset.url} alt="image" className='rounded-lg w-full'/>
-      {console.log(pin.image)}
+    <div className='m-2 '>
+      <div
+        onMouseEnter={()=> setOnImageHover(true)}
+        onMouseLeave={()=> setOnImageHover(false)}
+        onClick={() => navigate(`/pin-detail/${pin._id}`)}
+        className='relative  w-auto h-auto cursor-zoom-in hover:shadow-lg rounded-lg '
+      >
+        <img src={image.asset.url} alt="image" className='rounded-lg w-full'/> 
+
+        {onImageHover && 
+          <a 
+            href={`${image.asset.url}`}
+            download
+            onClick={(e)=>e.stopPropagation()}
+            className='absolute top-0 m-2 bg-white p-1 rounded-full opacity-60 hover:opacity-100'
+          >
+            <MdDownloadForOffline fontSize={20} className=''/>
+          </a>
+        }
+
+      </div>
+       
+      
     </div>
   )
 }
